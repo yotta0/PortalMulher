0eec47cdd77c228f18dd73b58e6be762758333b8
Davi Castro <davialmeida47jd@gmail.com>
2024-07-30 23:40:31 -0300

    feat(back-end): created migrate to alter column nullables

--- backend/core/models.py
+++ backend/core/models.py
@@ -9,11 +9,11 @@ class Contacts(models.Model):
     ]
 
     name = models.CharField(max_length=100)
-    description = models.TextField()
-    phone = models.CharField(max_length=20, blank=True)
-    email = models.EmailField(blank=True)
-    address = models.TextField(blank=True)
-    service_type = models.CharField(max_length=30, choices=SERVICE_CHOICES, default='resources')
+    description = models.TextField(blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    service_type = models.CharField(max_length=30, blank=True, choices=SERVICE_CHOICES, default='resources')
 
     class Meta:
         db_table = 'contacts'
